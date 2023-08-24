4c26acfe7351019adc767744ff383788002d1483
Abdelbar Amejjoud <abbelabbel9@gmail.com>
2023-08-25 00:07:31 +0100

    Update k

--- Amaze_Shell.c
+++ Amaze_Shell.c
@@ -1,69 +1,74 @@
 #include "shell.h"
-	int
-main(void)
+
+/**
+ * main - good code betty style.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	int i = 0;
-	char *line = NULL;
-	char *command = NULL;
-	char **arguments = NULL;
-	char *line_copy = NULL;
-	int interactive = isatty(STDIN_FILENO);
+    int i = 0;
+    char *line = NULL;
+    char *command = NULL;
+    char **arguments = NULL;
+    char *line_copy = NULL;
+    int interactive = isatty(STDIN_FILENO);
 
-	while (1)
-	{
-		if (interactive)
-		{
-			display_prompt();
-		}
-		line = read_command();
+    while (1)
+    {
+        if (interactive)
+        {
+            display_prompt();
+        }
+        line = read_command();
 
-		if (line == NULL)
-		{
-			break;
-		}
-		line_copy = strdup(line);
-		command = strtok(line_copy, " \t\n");
+        if (line == NULL)
+        {
+            break;
+        }
+        line_copy = strdup(line);
+        command = strtok(line_copy, " \t\n");
 
-		if (command != NULL)
-		{
-			arguments = pased_arguments(line);
+        if (command != NULL)
+        {
+            arguments = parsed_arguments(line);
 
-			if (strcmp(command, "exit") == 0)
-			{
-				while (arguments[i])
-				{
-					free(arguments[i]);
-					i++;
-				}
-				free(arguments);
-				free(line);
-				free(line_copy);
-				return (0);
-			}
-			else if (strcmp(command, "ls") == 0)
-			{
-				char command_path[] = "/bin/ls";
+            if (strcmp(command, "exit") == 0)
+            {
+                while (arguments[i])
+                {
+                    free(arguments[i]);
+                    i++;
+                }
+                free(arguments);
+                free(line);
+                free(line_copy);
+                return (0);
+            }
+            else if (strcmp(command, "ls") == 0)
+            {
+                char command_path[] = "/bin/ls";
 
-				execute_command(command_path, arguments, &line);
-			}
-			else if (strcmp(command, "env") == 0)
-			{
-				print_environment();
-			}
-			else
-			{
-				execute_command(command, arguments, &line);
-			}
-			i = 0;
-			while (arguments[i])
-			{
-				free(arguments[i]);
-				i++;
-			}
-			free(arguments);
-		}
-		free(line);
-		free(line_copy);
-	}
-	return (0);
+                execute_command(command_path, arguments, &line);
+            }
+            else if (strcmp(command, "env") == 0)
+            {
+                print_environment();
+            }
+            else
+            {
+                execute_command(command, arguments, &line);
+            }
+            i = 0;
+            while (arguments[i])
+            {
+                free(arguments[i]);
+                i++;
+            }
+            free(arguments);
+        }
+        free(line);
+        free(line_copy);
+    }
+    return (0);
 }

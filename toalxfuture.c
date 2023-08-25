d4e2d8e92db16982434d580947ea861ddc7584d1
Abdelbar Amejjoud <abbelabbel9@gmail.com>
2023-08-26 00:49:06 +0100

    Update this

--- toalxfuture.c
+++ toalxfuture.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 /**
- *find_executable_path - function finds that the path of an executable command
- *@command: name of the command
- *@path: a colon-separated list paths of to search from
- *Return: command_path or null
+ *find_executable_path - Design a function that locates the path of an executable command
+ *@command: commandi name
+ *@path: An array of paths separated by colons to be used for search
+ *Return: NULLL or command_path
  */
 char *find_executable_path(const char *command, const char *path)
 {
@@ -26,10 +26,10 @@ char *find_executable_path(const char *command, const char *path)
 	return (NULL);
 }
 /**
- *execute_command - function that executes a command with arguments
- *@command: command to be executed
- *@arguments: array of arguments for the command
- *@line: pointer to the original line of command
+ *execute_command - go get the command that creates
+ *@command: executed command
+ *@arguments: command of arrays to arguments
+ *@line: original line to be for the pointer
  */
 void execute_command(char *command, char **arguments, char **line)
 {
@@ -74,7 +74,7 @@ void execute_command(char *command, char **arguments, char **line)
 	}
 }
 /**
- *print_environment - function that prints the environment
+ *print_environment - enviroment that will be created with a fumction
  */
 void print_environment(void)
 {

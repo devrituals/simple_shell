375dd5d51e3a72d84db98c2a208814b894954f34
Abdelbar Amejjoud <abbelabbel9@gmail.com>
2023-08-26 01:03:18 +0100

    Update

--- shell.h
+++ shell.h
@@ -1,6 +1,5 @@
 #ifndef SHELL_H
 #define SHELL_H
-
 #define MAX_COMMAND_LENGTH 1024
 #define MAX_ARGUMENTS 64
 
@@ -13,11 +12,11 @@
 void execute_command(char *command, char **arguments, char **line);
 void print_environment(void);
 char *find_executable_path(const char *command, const char *path);
-char *get_recreated_env(const char *name);
-ssize_t get_line(char **lineptr, size_t *n, FILE *stream);
+char *recreated_getenv(const char *name);
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 extern char **environ;
 void display_prompt(void);
 char *read_command();
-char **parsed_arguments(char *line);
+char **pased_arguments(char *line);
 
 #endif
